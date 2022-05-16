6020f32b436e667c0621b1cac61e735da5b51ec9
Gabriela Vasconcelos <87244664+Gabriela8V@users.noreply.github.com>
2022-05-16 16:43:18 -0300

    Add files via upload

---
+++ agenda.sql
@@ -0,0 +1,16 @@
+/**
+   Agenda de contatos
+   @autor Gabriela
+*/
+
+-- Verificar bancos de dados
+show databases;
+
+-- Criar um novo banco de dados 
+create database teste;
+create database agendagabriela;
+-- Excluir um banco de dados 
+drop database teste;
+
+-- Selecionar o baco de dados 
+use agendagabriela;
